6db378c5e40770625891d30bb3bdc87ca60b4ee6
shardu mohite <shardul@weboniselab.com>
2014-01-15 22:52:32 +0530

    Home page update and seed data addition

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@
 
 puts "ROLES"
 YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({:name => role }, :without_protection => true)
-  puta 'Role : ' << role
+  Role.find_or_create_by_name(role)
+  puts 'Role : ' << role
 end
 
 puts 'DEFAULT USERS'
