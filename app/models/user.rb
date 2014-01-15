668f9c5ff54fd3a83c35a11ea54bf7ad8a1a8f9d
shardu mohite <shardul@weboniselab.com>
2014-01-15 21:04:04 +0530

    Update user model with migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+  
+  rolify
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  #attr_accessible :name , :email , :password , :password_confirmation ,:remember_me 
+  
 end
